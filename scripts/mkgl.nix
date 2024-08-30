3fa473d675f481f99075b69303d32a127a00e2c2
Frederik Richter <frederik.richter@mailbox.org>
2024-08-30 23:25:50 +0200

    added implementation of os detection in gl wrapper

--- scripts/mkgl.nix
+++ scripts/mkgl.nix
@@ -1,16 +1,21 @@
 pkgs:
 program:
 let
+currentOS = builtins.currentSystem;
 pname = program.meta.mainProgram;
-  wrapped = pkgs.writeShellScriptBin "${pname}" ''
-    exec nixGL ${program}/bin/${pname}
-  '';
+wrapped = pkgs.writeShellScriptBin "${pname}" ''
+exec nixGL ${program}/bin/${pname}
+'';
 in
-pkgs.symlinkJoin {
-  name = pname;
-  paths = [
-    wrapped
-    program
-  ];
-}
+# if not nixos patch with nixGL else return input
+if builtins.match "nixos" currentOS == null then
+	pkgs.symlinkJoin {
+	  name = pname;
+	  paths = [
+	    wrapped
+	    program
+	  ];
+	}
+else
+	program
 
