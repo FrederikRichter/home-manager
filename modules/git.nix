4800578f51f41bd0aaea8a7d19170bcdae2db95b
Frederik Richter <frederik.richter@mailbox.org>
2024-08-30 14:52:49 +0200

    Initial testing hm config

---
+++ modules/git.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Frederik Richter";
+    userEmail = "frederik.richter@mailbox.org";
+    aliases = {
+      st = "status";
+      cm = "commit";
+      co = "checkout";
+      br = "branch";
+      unstage = "reset HEAD --";
+      last = "log -1 HEAD";
+    };
+    extraConfig = {
+      core = {
+        editor = "$EDITOR";
+      };
+      color = {
+        ui = true;
+      };
+      pull = {
+        rebase = true;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+    };
+    ignores = [
+    ];
+  };
+}
---
+++ modules/zsh.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+	programs.zsh = {
+		enable = true;
+		enableCompletion = true;
+		autosuggestion.enable = true;
+		syntaxHighlighting.enable = true;
+		shellAliases = {
+			ll = "ls -l";
+			update = "home-manager switch";
+			sudo="sudo --preserve-env=PATH env";
+			tf="fuck";
+			hm-rollback="bash $(home-manager generations | fzf | awk -F '-> ' '{print $2 \"/activate\"}')";
+			hm="home-manager";
+		};
+		history = {
+			size = 10000;
+			path = "${config.xdg.dataHome}/zsh/history";
+		};
+		oh-my-zsh = {
+			enable = true;
+			plugins = [ "git" "thefuck" ];
+			theme = "eastwood";
+		};
+	};
+}
