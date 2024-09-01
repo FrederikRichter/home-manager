c62a2edf8e3b09f39c47d0fa9d4be31ba47ea81a
Frederik Richter <frederik.richter@mailbox.org>
2024-09-01 16:41:47 +0200

    fixed ssh key loading

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,16 +1,11 @@
 { config, pkgs, libs, ... }:
 {
-    # copy ssh keys into the right directory
-    home.file = {
-        ".ssh/id_rsa".source = ../secrets/ssh/id_rsa;
-        ".ssh/id_rsa.pub".source = ../secrets/ssh/id_rsa.pub;
-    };
-
     programs.ssh = {
         enable = true;
         compression = true;
         forwardAgent = true;
         serverAliveInterval = 60;
+        addKeysToAgent = "yes";
         matchBlocks = {
             "github.com" = {
                 hostname = "github.com";
