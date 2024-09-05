96f3b31be382de0469fc309e7f53055a9ef8a60b
Frederik Richter <frederik.richter@mailbox.org>
2024-09-05 20:03:01 +0200

    proper zsh update alias escape and better sway conf

--- modules/sway.nix
+++ modules/sway.nix
@@ -25,6 +25,32 @@ in
             # bars = [{"command" = "${waybar}/bin/waybar";}];
 
             # keybindings
+            keybindings = mkOptionDefault {
+            Print = ''exec ${grim}/bin/grim -g "$(${slurp}/bin/slurp -d)" - | ${wl-clipboard}/bin/wl-copy -t image/png'';
+            "${modifier}+q" = "kill";
+            "${modifier}+${left}" = "focus left; exec $movemouse";
+            "${modifier}+${down}" = "focus down; exec $movemouse";
+            "${modifier}+${up}" = "focus up; exec $movemouse";
+            "${modifier}+${right}" = "focus right; exec $movemouse";
+
+            "${modifier}+Shift+${left}" = "move left";
+            "${modifier}+Shift+${down}" = "move down";
+            "${modifier}+Shift+${up}" = "move up";
+            "${modifier}+Shift+${right}" = "move right";
+
+            "${modifier}+a" = "workspace number 1";
+            "${modifier}+b" = "workspace number 2";
+            "${modifier}+c" = "workspace number 3";
+            "${modifier}+d" = "workspace number 4";
+
+            "${modifier}+Shift+a" = "move container to workspace number 1";
+            "${modifier}+Shift+b" = "move container to workspace number 2";
+            "${modifier}+Shift+c" = "move container to workspace number 3";
+            "${modifier}+Shift+d" = "move container to workspace number 4";
+
+            "${modifier}+Shift+r" = "restart";
+            };
+
         };
         extraConfig = ''
             input "type:keyboard" {
