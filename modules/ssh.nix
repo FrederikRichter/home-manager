528ff071c073cd9eb541b5aa5ca098d4d3d079e5
Frederik Richter <55528181+FrederikRichter@users.noreply.github.com>
2024-09-05 14:12:25 +0200

    Update ssh.nix

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -6,17 +6,6 @@
         forwardAgent = true;
         serverAliveInterval = 60;
         addKeysToAgent = "yes";
-        matchBlocks = {
-            "github.com" = {
-                hostname = "github.com";
-                user = "git";
-                identityFile = "~/.ssh/id_rsa";
-                extraOptions = {
-                    PreferredAuthentications = "publickey";
-                    IdentitiesOnly = "yes";
-                };
-            };
-        };
     };
     services.ssh-agent = {
         enable = true;
