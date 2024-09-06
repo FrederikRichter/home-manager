049c2e4e6aa375f72082417706326e04fccc2fc3
Frederik Richter <frederik.richter@mailbox.org>
2024-09-06 19:15:21 +0200

    added font but doesnt seem to change anything

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,13 +1,22 @@
 { config, pkgs, libs, ... }:
 {
     stylix = {
-    enable = true;
-    autoEnable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-    image = pkgs.fetchurl {
-        url = "https://github.com/NixOS/nixos-artwork/blob/master/wallpapers/nix-wallpaper-dracula.png?raw=true";
-        sha256 = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
-    };
+        enable = true;
+        autoEnable = true;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+        image = pkgs.fetchurl {
+            url = "https://github.com/NixOS/nixos-artwork/blob/master/wallpapers/nix-wallpaper-dracula.png?raw=true";
+            sha256 = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
+        };
+        fonts = {
+            monospace = {
+                package = pkgs.iosevka-comfy.comfy-wide-duo;
+                name = "Iosevka comfy wide motion duo";
+            };
+            serif = config.stylix.fonts.monospace;
+            sansSerif = config.stylix.fonts.monospace;
+            emoji = config.stylix.fonts.monospace;
+        };
     };
 
 }
