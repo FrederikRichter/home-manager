eebff17620809d4f13d1b5e9cc32e0352ae497e5
Frederik Richter <frederik.richter@mailbox.org>
2024-09-08 14:59:48 +0200

    added megasync service

--- modules/mega.nix
+++ modules/mega.nix
@@ -1,4 +1,20 @@
 { config, pkgs, libs, ... }:
 {
-    home.packages = [megacmd];
-;}
+    home.packages = [pkgs.megacmd];
+    systemd.user.services.mega = {
+        Unit = {
+            Description = "mega-sync";
+            After = [ "graphical-session-pre.target" ];
+            PartOf = [ "graphical-session.target" ];
+        };
+
+        Service = {
+            ExecStart = "${pkgs.megacmd}/bin/mega-sync";
+            Restart = "on-failure";
+        };
+
+        Install = {
+            WantedBy = [ "graphical-session.target" ];
+        };
+    };
+}
