7fb1f73cdc726191cbf5a131813e6ff567ee0c80
Frederik Richter <frederik.richter@mailbox.org>
2024-09-09 17:19:32 +0200

    fixed kanshi config

--- modules/kanshi.nix
+++ modules/kanshi.nix
@@ -1,10 +1,37 @@
 {
     lib,
-    config,
+        config,
         ...
 }:
 {
     services.kanshi = {
-            enable = true;
-        };
+        enable = true;
+        settings = [
+        {
+            profile.name = "undocked";
+            profile.outputs = [
+            {
+                criteria = "eDP-1";
+                status = "enable";
+            }
+            ];
+        }
+        {
+            profile.name = "docked";
+            profile.outputs = [
+            {
+                criteria = "eDP-1";
+                status = "disable";
+            }
+            {
+                criteria = "HDMI-A-1";
+                status = "enable";
+                mode = "2560x1440@60Hz";
+                position = "0,0";
+                scale = 1.5;
+            }
+            ];
+        }
+        ];
+    };
 }
