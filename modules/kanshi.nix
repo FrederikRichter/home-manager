83f7518d5c6578e946a7f64a7c868d0921fde463
Frederik Richter <frederik.richter@mailbox.org>
2024-09-09 17:27:06 +0200

    fixed kanshi scaling

--- modules/kanshi.nix
+++ modules/kanshi.nix
@@ -28,7 +28,6 @@
                 status = "enable";
                 mode = "2560x1440@60Hz";
                 position = "0,0";
-                scale = 1.5;
             }
             ];
         }
