e6d7ba91df53dda1c042f821c77948dbbfb0afea
Frederik Richter <frederik.richter@mailbox.org>
2024-09-10 15:44:08 +0200

    google udm 14

--- modules/qutebrowser.nix
+++ modules/qutebrowser.nix
@@ -8,7 +8,7 @@
         };
         searchEngines = {
                 w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
-                DEFAULT = "https://www.google.com/search?hl=en&q={}";
+                DEFAULT = "https://www.google.com/search?q={}&udm=14";
                 hm = "https://home-manager-options.extranix.com/?query={}&release=release-24.05";
                 np = "https://search.nixos.org/packages?channel=24.05&from=0&size=50&sort=relevance&type=packages&query={}";
                 pp = "https://www.perplexity.ai/search/?q={}";
@@ -16,6 +16,7 @@
         };
         settings = {
             tabs.show = "never";
+            url.start_pages = "https://udm14.com/";
             };
         extraConfig = ''
             config.unbind("H", mode="normal")
