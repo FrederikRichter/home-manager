878e9554e91d4a3d9f95eaf5b590c8123afe9b60
Frederik Richter <frederik.richter@mailbox.org>
2024-09-15 00:27:57 +0200

    added some xdg desktop apps

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,12 +1,43 @@
-{config, lib, ...}:
+{config, lib, pkgs, ...}:
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-    "x-scheme-handler/http" = ["qutebrowser.desktop"];
-    "x-scheme-handler/https" = ["qutebrowser.desktop"];
-    "text/html" = ["qutebrowser.desktop"];
-    "application/pdf" = ["zathura.desktop"];
+    xdg = {
+        enable = true;
+        mime.enable = true;
+        mimeApps = {
+            enable = true;
+            defaultApplications = {
+                "x-scheme-handler/http" = ["qutebrowser.desktop"];
+                "x-scheme-handler/https" = ["qutebrowser.desktop"];
+                "text/html" = ["qutebrowser.desktop"];
+                "text/plain" = ["nvim.desktop"];
+                "application/pdf" = ["zathura.desktop"];
+            };
+        };
+        desktopEntries = {
+            nvim = {
+                name = "neovim from flake";
+                genericName = "text editor";
+                exec = "${pkgs.kitty}/bin/kitty -e nvim %U";
+                terminal = true;
+                categories = [  ];
+                mimeType = [ "text/plain" ];
+            };
+            qutebrowser = {
+                name = "qutebrowser";
+                genericName = "webbrowser";
+                exec = "${pkgs.qutebrowser}/bin/qutebrowser %U";
+                terminal = false;
+                categories = [  ];
+                mimeType = [ "text/html" "text/xml"];
+            };
+            zathura = {
+                name = "zathura";
+                genericName = "pdf viewer";
+                exec = "${pkgs.zathura}/bin/zathura %U";
+                terminal = false;
+                categories = [  ];
+                mimeType = [ "application/pdf" ];
+            };
+        };
     };
-  };
 }
