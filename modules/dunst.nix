4b640eb7fd480e65b431ba2b39084d1988cc9b60
Frederik Richter <frederik.richter@mailbox.org>
2024-09-14 23:48:13 +0200

    tried settings dunst border but fails for some reason

--- modules/dunst.nix
+++ modules/dunst.nix
@@ -1,6 +1,20 @@
 {config, lib, ...}:
 {
-services.dunst = {
+    services.dunst = {
         enable = true;
+        settings = lib.mkDefault {
+            global = {
+                frame_width = 1;
+            };
+            urgency_low = {
+                frame_color = "#000000";
+            };
+            urgency_normal = {
+                frame_color = "#000000";
+            };
+            urgency_critical = {
+                frame_color = "#000000";
+            };
+        };
     };
 }
