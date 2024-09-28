b58649f45e9a8a79578a80af14e02bc41d8f773a
Frederik Richter <frederik.richter@mailbox.org>
2024-09-28 19:23:35 +0200

    added mpv config

---
+++ modules/mpv.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, config, options, ... }:
+{
+    programs.mpv = {
+            enable = true;
+            config = ''
+                profile = "gpu-hq";
+                hwdec="auto-safe";
+                vo="gpu";
+                ytdl-format = "bestvideo+bestaudio";
+                cache-default = 4000000;
+                gpu-context=wayland;
+            '';
+        };
+}
