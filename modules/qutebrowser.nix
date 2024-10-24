640cdc108c41e61d03f2c3ad2722b189660225d1
Frederik Richter <frederik.richter@mailbox.org>
2024-10-24 20:37:17 +0200

    moved qutebrowser search engines to new branch

--- modules/qutebrowser.nix
+++ modules/qutebrowser.nix
@@ -10,8 +10,8 @@
         searchEngines = {
                 w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
                 DEFAULT = "https://www.google.com/search?q={}&udm=14";
-                hm = "https://home-manager-options.extranix.com/?query={}&release=release-24.05";
-                np = "https://search.nixos.org/packages?channel=24.05&from=0&size=50&sort=relevance&type=packages&query={}";
+                hm = "https://home-manager-options.extranix.com/?query={}&release=master";
+                np = "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={}";
                 pp = "https://www.perplexity.ai/search/?q={}";
                 yt = "https://www.youtube.com/results?search_query={}";
         };
