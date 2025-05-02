87c330ee228a34d09b75ec22a76c80b5e7aac16b
Frederik Richter <frederik.richter@mailbox.org>
2025-05-02 12:38:40 +0200

    add tu ssh config

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -7,7 +7,15 @@
         serverAliveInterval = 60;
         addKeysToAgent = "yes";
         matchBlocks = {
-            "gitlab-tu" = {
+            "gitlab-tu-cs" = {
+                hostname = "git.cg.cs.tu-bs.de";
+                user = "gogs";
+                identityFile = "~/.ssh/id_rsa";
+                extraOptions = {
+                    identitiesOnly = "yes";
+                };
+            };
+            "gitlab-tu-rz" = {
                 hostname = "git.rz.tu-bs.de";
                 user = "git";
                 identityFile = "~/.ssh/id_rsa";
@@ -18,22 +26,6 @@
             "github.com" = {
                 hostname = "github.com";
                 user = "git";
-                identityFile = "~/.ssh/id_rsa";
-                extraOptions = {
-                    identitiesOnly = "yes";
-                };
-            };
-            "nix-server" = {
-                hostname = "192.168.1.189";
-                user = "frederik";
-                identityFile = "~/.ssh/id_rsa";
-                extraOptions = {
-                    identitiesOnly = "yes";
-                };
-            };
-            "nix-remote-builder" = {
-                hostname = "192.168.1.189";
-                user = "nixremote";
                 identityFile = "~/.ssh/id_rsa";
                 extraOptions = {
                     identitiesOnly = "yes";
