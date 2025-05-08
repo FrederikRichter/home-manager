03219697b3c236be8ae795d0e77cd6e907503c6b
Frederik Richter <frederik.richter@mailbox.org>
2025-05-08 12:12:32 +0200

    replace: kanshi -> shikane

--- modules/kanshi.nix
+++ modules/kanshi.nix
@@ -1,36 +1,43 @@
-{
-    lib,
-    config,
-    ...
-}:
-{
-    services.kanshi = {
-        enable = true;
-        settings = [
-        {
-            profile.name = "undocked";
-            profile.outputs = [
-            {
-                criteria = "eDP-1";
-                status = "enable";
-            }
-            ];
-        }
-        {
-            profile.name = "docked";
-            profile.outputs = [
-            {
-                criteria = "eDP-1";
-                status = "disable";
-            }
-            {
-                criteria = "Xiaomi Corporation Mi Monitor 5744900021637";
-                status = "enable";
-                mode = "2560x1440@59.951Hz";
-                position = "0,0";
-            }
-            ];
-        }
-        ];
-    };
-}
+# {
+#     lib,
+#     config,
+#     ...
+# }:
+# {
+#     services.kanshi = {
+#         enable = true;
+#         settings = [
+#         {
+#             profile.name = "Xiaomi";
+#             profile.outputs = [
+#             {
+#                 criteria = "eDP-1";
+#                 status = "disable";
+#             }
+#             {
+#                 criteria = "Xiaomi Corporation Mi Monitor 5744900021637";
+#                 status = "enable";
+#                 mode = "2560x1440@59.951Hz";
+#                 position = "0,0";
+#             }
+#             ];
+#         }
+#         {
+#             profile.name = "Default HDMI";
+#             profile.outputs = [
+#             {
+#                 criteria = "eDP-1";
+#                 status = "enable";
+#                 mode = "1920x1080@60Hz";
+#             }
+#             {
+#                 criteria = "HDMI-A-1";
+#                 status = "enable";
+#                 position = "0,0";
+#                 mode = "1920x1080@60Hz";
+#             }
+#             ];
+#             }
+#         ];
+#     };
+# }
---
+++ modules/shikane.nix
@@ -0,0 +1,53 @@
+{}:
+{
+    services.shikane = {
+        enable = true;
+        settings = {
+            profile = [
+            {
+                name = "external-monitor-default";
+                output = [
+                {
+                    match = "eDP-1";
+                    enable = true;
+                }
+                {
+                    match = "HDMI-A-1";
+                    enable = true;
+                    position = {
+                        x = 0;
+                        y = 0;
+                    };
+                }
+                ];
+            }
+            {
+                name = "docked Xiaomi";
+                output = [
+                {
+                    match = "eDP-1";
+                    enable = false;
+                }
+                {
+                    match = "DP-4";
+                    enable = true;
+                    position = {
+                        x = 0;
+                        y = 0;
+                    };
+                }
+                ];
+            }
+            {
+                name = "builtin-monitor-only";
+                output = [
+                {
+                    match = "eDP-1";
+                    enable = true;
+                }
+                ];
+            }
+            ];
+        };
+    };
+}
