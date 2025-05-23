e4842e103dd5c0a6dbf35f793e4f2c3e7460c274
Frederik Richter <frederik.richter@mailbox.org>
2025-05-23 17:33:38 +0200

    style: switch to gb light

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,7 +3,7 @@
     stylix = {
         enable = true;
         autoEnable = true;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-hard.yaml";
         image = pkgs.fetchurl {
             url = "https://github.com/NixOS/nixos-artwork/blob/master/wallpapers/nix-wallpaper-dracula.png?raw=true";
             sha256 = "sha256-SykeFJXCzkeaxw06np0QkJCK28e0k30PdY8ZDVcQnh4=";
