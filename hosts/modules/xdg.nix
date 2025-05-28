b73be182955ca61597ee1808ab7063f55cb8d16a
Frederik Richter <frederik.richter@mailbox.org>
2025-05-29 01:41:58 +0200

    add: xdg creates dirs now

--- hosts/modules/xdg.nix
+++ hosts/modules/xdg.nix
@@ -3,21 +3,16 @@
     xdg = {
         enable = true;
         mime.enable = true;
+        userDirs.enable = true;
         mimeApps = {
             enable = true;
             defaultApplications = {
                 "x-scheme-handler/http" = "qutebrowser.desktop";
                 "x-scheme-handler/https" = "qutebrowser.desktop";
-                "text/html" = "qutebrowser.desktop";
+                "text/html" = "librewolf.desktop";
                 "text/plain" = "nvim.desktop";
                 "application/pdf" = "zathura.desktop";
                 "document/pdf" = "zathura.desktop";
-            };
-            associations.removed = {
-                "application/pdf" = "inkscape.desktop";
-                "document/pdf" = "inkscape.desktop";
-                "x-scheme-handler/https" = "firefox.desktop";
-                "x-scheme-handler/http" = "firefox.desktop";
             };
         };
 
@@ -32,4 +27,11 @@
             };
         };
     };
+      home.file."Desktop".source = null;
+      home.file."Downloads".source = null;
+      home.file."Videos".source = null;
+      home.file."Documents".source = null;
+      home.file."Music".source = null;
+      home.file."Pictures".source = null;
+      home.file."git".source = null;
 }
