2666eec97c478b0589e1ab3938640ae01e886138
Frederik Richter <frederik.richter@mailbox.org>
2025-05-31 13:14:49 +0200

    keepass: enable browser integration

--- modules/keepass.nix
+++ modules/keepass.nix
@@ -43,7 +43,8 @@
 
                 [Security]
                 LockDatabaseScreenLock=false
-                    '';
+                ClearClipboardTimeout=40
+                '';
         };
     };
 
