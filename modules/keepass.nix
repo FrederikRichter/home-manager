6350e7631bb18f296c6faa000a59a2f88d4c6024
Frederik Richter <frederik.richter@mailbox.org>
2025-05-31 13:37:54 +0200

    fix: keepassxc browser integration

--- modules/keepass.nix
+++ modules/keepass.nix
@@ -41,12 +41,6 @@
                 AdditionalChars=
                 ExcludedChars=
 
-                [KeeShare]
-                Active="<?xml version=\"1.0\"?>\n<KeeShare xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <Active/>\n</KeeShare>\n"
-                    Foreign="<?xml version=\"1.0\"?>\n<KeeShare xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <Foreign/>\n</KeeShare>\n"
-                    Own="<?xml version=\"1.0\"?>\n<KeeShare xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <PrivateKey/>\n  <PublicKey/>\n</KeeShare>\n"
-                    QuietSuccess=true
-
                 [Security]
                 LockDatabaseScreenLock=false
                 ClearClipboardTimeout=40
--- modules/librewolf.nix
+++ modules/librewolf.nix
@@ -10,4 +10,15 @@
             "browser.toolbars.bookmarks.visibility" = "never";
         };
     };
+    home.file."/.librewolf/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".text = ''
+    {
+        "allowed_extensions": [
+            "keepassxc-browser@keepassxc.org"
+        ],
+        "description": "KeePassXC integration with native messaging support",
+        "name": "org.keepassxc.keepassxc_browser",
+        "path": "${pkgs.keepassxc}/bin/keepassxc-proxy",
+        "type": "stdio"
+    }
+    '';
 }
