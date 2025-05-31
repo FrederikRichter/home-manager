5e8492449cd4a5279e15b29b8e09d93e90cb8675
Frederik Richter <frederik.richter@mailbox.org>
2025-05-31 14:29:31 +0200

    rm: thunderbird

--- modules/mailspring.nix
+++ modules/mailspring.nix
@@ -1,5 +1,58 @@
 {pkgs, ...}:
 {
+    accounts.email.accounts = {
+        mailbox = {
+            address = "frederik.richter@mailbox.org";
+            maildir.path = "mailbox";
+            realName = "Frederik Richter";
+            primary = true;
+            userName = "frederik.richter@mailbox.org";
+            imap = {
+                host = "imap.mailbox.org";
+                port = 993;
+            };
+            smtp = {
+                host = "smtp.mailbox.org";
+                port = 465;
+            };
+            aliases = [
+                "fredster@mailbox.org"
+                "frederik.shopping@mailbox.org"
+                "frederik.gaming@mailbox.org"
+            ];
+        };
+        tu-bs = {
+            address = "frederik.richter@tu-braunschweig.de";
+            maildir.path = "tubs";
+            realName = "Frederik Richter";
+            userName = "AD\\y0117852";
+            imap = {
+                host = "mail.tu-braunschweig.de";
+                port = 993;
+            };
+            smtp = {
+                host = "mail.tu-braunschweig.de";
+                port = 587;
+                tls.useStartTls = true;
+            };
+            };
+
+        gmail = {
+            address = "frederik.richter@tu-braunschweig.de";
+            maildir.path = "tubs";
+            realName = "Frederik Richter";
+            userName = "frederik.twix@gmail.com";
+            imap = {
+                host = "imap.gmail.com";
+                port = 993;
+            };
+            smtp = {
+                host = "smtp.gmail.com";
+                port = 465;
+            };
+        };
+    };
+
     xdg.desktopEntries.mailspring = {
       name = "Mailspring";
       exec = "${pkgs.mailspring}/bin/mailspring --password-store=gnome-libsecret";
@@ -8,4 +61,5 @@
       comment = "A beautiful, modern email client";
       categories = [ "Network" "Email" ];
     };
+
 }
