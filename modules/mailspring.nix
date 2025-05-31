8af440b958a2828605eccbb43af32b1e60012d1d
Frederik Richter <frederik.richter@mailbox.org>
2025-05-31 13:16:39 +0200

    add: mailspring config

---
+++ modules/mailspring.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}:
+{
+    xdg.desktopEntries.mailspring = {
+      name = "Mailspring";
+      exec = "${pkgs.mailspring}/bin/mailspring --password-store=gnome-libsecret";
+      icon = "mailspring";
+      terminal = false;
+      comment = "A beautiful, modern email client";
+      categories = [ "Network" "Email" ];
+    };
+
+    services.gnome-keyring.enable = true;
+
+}
