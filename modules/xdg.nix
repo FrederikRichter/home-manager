a01d5d27cc1f6d3eb23e1baa9e8d28ca4ab7c6b5
Frederik Richter <frederik.richter@mailbox.org>
2025-06-24 01:19:28 +0200

    xdg: remove unwanted associations

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -5,28 +5,28 @@ xdg = {
   mime.enable = true;
   userDirs.enable = true;
 
-  mimeApps = {
-    enable = true;
-
-    defaultApplications = {
-      "x-scheme-handler/http" = "librewolf.desktop";
-      "x-scheme-handler/https" = "librewolf.desktop";
-      "text/html" = "librewolf.desktop";
-      "text/plain" = "nvim.desktop";
-      "application/pdf" = "zathura.desktop";
-      "document/pdf" = "zathura.desktop";
-      "inode/directory" = "nautilus.desktop";
-    };
-
-    associations.added = {
-      "x-scheme-handler/http" = [ "librewolf.desktop" ];
-      "x-scheme-handler/https" = [ "librewolf.desktop" ];
-      "text/html" = [ "librewolf.desktop" ];
-      "text/plain" = [ "nvim.desktop" ];
-      "application/pdf" = [ "zathura.desktop" ];
-      "document/pdf" = [ "zathura.desktop" ];
-      "inode/directory" = [ "nautilus.desktop" ];
-    };
-  };
+  # mimeApps = {
+  #   enable = true;
+  #
+  #   defaultApplications = {
+  #     "x-scheme-handler/http" = "librewolf.desktop";
+  #     "x-scheme-handler/https" = "librewolf.desktop";
+  #     "text/html" = "librewolf.desktop";
+  #     "text/plain" = "nvim.desktop";
+  #     "application/pdf" = "zathura.desktop";
+  #     "document/pdf" = "zathura.desktop";
+  #     "inode/directory" = "nautilus.desktop";
+  #   };
+  #
+  #   associations.added = {
+  #     "x-scheme-handler/http" = [ "librewolf.desktop" ];
+  #     "x-scheme-handler/https" = [ "librewolf.desktop" ];
+  #     "text/html" = [ "librewolf.desktop" ];
+  #     "text/plain" = [ "nvim.desktop" ];
+  #     "application/pdf" = [ "zathura.desktop" ];
+  #     "document/pdf" = [ "zathura.desktop" ];
+  #     "inode/directory" = [ "nautilus.desktop" ];
+  #   };
+  # };
 };
 }
