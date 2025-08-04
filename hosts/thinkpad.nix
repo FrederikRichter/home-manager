1bf7423312fdd8f53ee12169160802a89eb94971
Frederik Richter <frederik.richter@mailbox.org>
2025-08-04 10:50:37 +0200

    stylix: wallpaper

--- hosts/thinkpad.nix
+++ hosts/thinkpad.nix
@@ -8,12 +8,16 @@
     shikane.enable = true;
     sway.enable = true;
     swaylock.enable = true;
+    hyprland.enable = true;
     
     programs.waybar = {
-        systemd.target = "sway-session.target";
+        systemd.target = "hyprland-session.target";
         settings.mainBar = {
-            modules-left = [ "sway/workspaces" "idle_inhibitor" ];
+            modules-left = [ "hyprland/workspaces" "idle_inhibitor" ];
             modules-right = ["pulseaudio" "network" "battery" "cpu" "memory" "disk" "clock" "tray"];
         };
     };
+    wayland.windowManager.hyprland.settings.input.kb_layout = "de";
+
+    stylix.image = ../wallpaper/akira.jpg;
 }
