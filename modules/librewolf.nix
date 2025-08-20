079e195c04fdf4d810132640ef353696ba3057eb
Frederik Richter <frederik.richter@mailbox.org>
2025-08-20 16:56:46 +0200

    librewolf: remove declarative config

--- modules/librewolf.nix
+++ modules/librewolf.nix
@@ -1,16 +1,5 @@
 { config, pkgs, lib, ... }:
 {
-    programs.librewolf = {
-        enable = true;
-        settings = {
-            "browser.sessionstore.resume_from_crash" = false;
-            "browser.search.searchEnginesURL" = "https://search.hbubli.cc/";
-            "browser.policies.runOncePerModification.setDefaultSearchEngine" =  "https://search.hbubli.cc/";
-            "browser.search.separatePrivateDefault" = false;
-            "browser.toolbars.bookmarks.visibility" = "never";
-        };
-    };
-
     home.file."/.librewolf/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".text = ''
     {
         "allowed_extensions": [
