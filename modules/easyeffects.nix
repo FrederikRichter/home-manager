0c2e20319c117b1c592b34c0903567919dfb308e
Frederik Richter <frederik.richter@mailbox.org>
2025-09-05 18:40:36 +0200

    modules: add easyeffects service

---
+++ modules/easyeffects.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}:
+{
+systemd.user.services.easyeffects = {
+    Unit = {
+        Description = "Easyeffects service";
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+        ExecStart = "${pkgs.easyeffects}/bin/easyeffects --gapplication-service";
+        Restart = "on-failure";
+    };
+
+    Install = {
+        WantedBy = [ "graphical-session.target" ];
+    };
+};
+}
