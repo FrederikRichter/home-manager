67ff581a14c06c279da1c49895b26ba74302d5de
Frederik Richter <frederik.richter@mailbox.org>
2025-10-31 13:47:12 +0100

    battlestation: set highres display settings

--- hosts/battlestation.nix
+++ hosts/battlestation.nix
@@ -17,7 +17,7 @@ wayland.windowManager.hyprland = {
     settings = {
         input.kb_layout = "us";
         monitor = [
-            ", preferred,auto,1,bitdepth,10,vrr,1,cm,hdr,sdrbrightness, 1.3, sdrsaturation, 1.15"
+            ", highres,auto,1,bitdepth,10,vrr,1,cm,hdr,sdrbrightness, 1.3, sdrsaturation, 1.15"
         ];
         experimental = {
             xx_color_management_v4 = true;
