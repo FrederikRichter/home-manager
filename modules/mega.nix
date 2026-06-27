3b75731846151f37937d09f8166119641592d3e2
Frederik Richter <frederik.richter@mailbox.org>
2026-06-27 09:43:33 +0200

    mega: disable

--- modules/mega.nix
+++ modules/mega.nix
@@ -1,21 +1,21 @@
 { config, pkgs, libs, ... }:
 {
-    home.packages = [pkgs.megacmd];
-    systemd.user.services.mega = {
-        Unit = {
-            Description = "mega-cmd server";
-            After = [ "network.target" ];
-            PartOf = [ "graphical-session.target" ];
-        };
-
-        Service = {
-            ExecStart = "${pkgs.megacmd}/bin/mega-cmd-server";
-            Restart = "always";
-            Environment = "PATH=${pkgs.megacmd}/bin/";
-        };
-
-        Install = {
-            WantedBy = [ "graphical-session.target" ];
-        };
-    };
+    # home.packages = [pkgs.megacmd];
+    # systemd.user.services.mega = {
+    #     Unit = {
+    #         Description = "mega-cmd server";
+    #         After = [ "network.target" ];
+    #         PartOf = [ "graphical-session.target" ];
+    #     };
+    #
+    #     Service = {
+    #         ExecStart = "${pkgs.megacmd}/bin/mega-cmd-server";
+    #         Restart = "always";
+    #         Environment = "PATH=${pkgs.megacmd}/bin/";
+    #     };
+    #
+    #     Install = {
+    #         WantedBy = [ "graphical-session.target" ];
+    #     };
+    # };
 }
